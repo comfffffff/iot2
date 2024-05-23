23245f281768a3446fce515e473f105d1631362b
Choiseonghyun <chltmdgus272@gmail.com>
2024-05-23 20:55:53 +0900

    callback and closer

--- 5-5.js
+++ 5-5.js
@@ -8,3 +8,15 @@ var outer = (function () {
 console.log(outer());
 console.log(outer());
 outer = null;
+
+var a = 0;
+var intervalId = null;
+var inner = function () {
+    if (++a >= 10) {
+        clearInterval(intervalId);
+        inner = null; 
+    }
+    console.log(a);
+};
+intervalId = setInterval(inner, 1000);
+}) ();
